11a25294012ab38bfb98d2a265b7654ac087a702
Alexander Neumann <alexander.neumann@picos-software.com>
2010-10-14 12:33:36 +0200

    Adapt to log package changes in release 2010-10-13

--- conn_log.go
+++ conn_log.go
@@ -13,11 +13,11 @@ import (
 )
 
 func (conn *Conn) log(level LogLevel, v ...interface{}) {
-	log.Stdout(v...)
+	log.Print(v...)
 }
 
 func (conn *Conn) logf(level LogLevel, format string, v ...interface{}) {
-	log.Stdoutf(format, v...)
+	log.Printf(format, v...)
 }
 
 func (conn *Conn) logError(level LogLevel, err os.Error) {
