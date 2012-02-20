68715a5ff9001d5045d5fe62b63ee28052546e8e
Alexander Neumann <an2048@googlemail.com>
2012-02-20 20:48:55 +0100

    Add authors file and adjust license

--- util.go
+++ util.go
@@ -1,4 +1,4 @@
-// Copyright 2010 Alexander Neumann. All rights reserved.
+// Copyright 2010 The go-pgsql Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
