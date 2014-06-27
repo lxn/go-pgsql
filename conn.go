c611751049fb3a26ec16bcd98a22ab7ceae5a775
Oscar Ayoun <oayoun@autopassgroup.com>
2014-06-27 18:10:19 +0200

    trying modif

--- conn.go
+++ conn.go
@@ -304,7 +304,7 @@ func Connect(connStr string, logLevel LogLevel) (conn *Conn, err error) {
 		params.User = env
 	}
 
-	newConn.log(LogPanic, "FIRST LOG")
+	newConn.log(newConn.LogDebug, "FIRST LOG")
 	tcpConn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", params.Host, params.Port))
 	panicIfErr(err)
 
